b071863a7edba7e8ff2e46bb146caa873edb6bb8
Vincent K Edison <vk@luxodev.com>
2021-08-13 10:32:14 +0700

    Support emit showModal from a modal

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,6 +1,6 @@
 <div id="laravel-livewire-modals" class="modal fade" tabindex="-1"
     data-bs-backdrop="static" data-bs-keyboard="false" wire:ignore.self>
     @if($alias)
-        @livewire($alias, $params)
+        @livewire($alias, $params, key($alias))
     @endif
 </div>
