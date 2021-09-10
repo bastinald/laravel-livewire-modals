fd53779095ff15d201a2adabdfbde4d101b13d59
bastinald <bastinald@icloud.com>
2021-09-09 20:08:07 -0400

    v3

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,6 +1,9 @@
-<div id="laravel-livewire-modals" class="modal fade" tabindex="-1"
-    data-bs-backdrop="static" data-bs-keyboard="false" wire:ignore.self>
+<div id="laravel-livewire-modals" tabindex="-1"
+    data-bs-backdrop="static" data-bs-keyboard="false"
+    wire:ignore.self class="modal fade">
+
     @if($alias)
         @livewire($alias, $params, key($alias))
     @endif
+
 </div>
