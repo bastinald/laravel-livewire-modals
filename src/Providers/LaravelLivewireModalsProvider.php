6dddb72d7918de0a69acc36615548feb0cd778a1
Luanar Developers <90185393+luanardev@users.noreply.github.com>
2022-02-17 10:06:18 +0200

    Update LaravelLivewireModalsProvider.php

--- src/Providers/LaravelLivewireModalsProvider.php
+++ src/Providers/LaravelLivewireModalsProvider.php
@@ -10,13 +10,49 @@ class LaravelLivewireModalsProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'laravel-livewire-modals');
 
         $this->publishes(
             [__DIR__ . '/../../resources/views' => resource_path('views/vendor/laravel-livewire-modals')],
             ['laravel-livewire-modals', 'laravel-livewire-modals:views']
         );
 
-        Livewire::component('modals', Modals::class);
+
     }
+    
+    private function registerViews(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'laravel-livewire-modals');
+    }
+    
+    private function registerLivewireComponents(): void
+    {
+         Livewire::component('modals', Modals::class);
+    }
+
+    private function registerDirectives()
+    {
+        Blade::directive('laravelLivewireModalScript', function () {
+            return '<script src="' . asset("/vendor/laravel-livewire-modal/modal.js") . '"></script>';
+        });
+    }
+
+    private function registerPublishables(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/livewire-modal-twitter'),
+            ], 'livewire-modal-twitter:views');
+
+            $this->publishes([
+                __DIR__ . '/../resources/js' => resource_path('js/vendor/livewire-modal-twitter'),
+            ], 'livewire-modal-twitter:script');
+
+            $this->publishes([
+                __DIR__ . '/../public' => public_path('vendor/livewire-modal-twitter'),
+            ], 'livewire-modal-twitter:public');
+        }
+    }
+
+    
+
 }
