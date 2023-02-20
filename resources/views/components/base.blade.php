4c9455570e4054e1b0c645172c08a5939241e40a
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-20 17:03:01 +0200

    add modals

---
+++ resources/views/components/base.blade.php
@@ -0,0 +1,20 @@
+@props(['title' => null, 'footer' => null])
+<!-- Modal -->
+<div class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
